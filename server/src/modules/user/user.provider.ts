c7470ee6247f1d3edb99fbe783e7ee148e5901eb
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-25 12:20:01 +0300

    Remove Server

--- server/src/modules/user/user.provider.ts
+++
@@ -1,9 +0,0 @@
-import { USER_REPOSITORY } from 'src/core/constants';
-import { User } from './models/user.model';
-
-export const userProvider = [
-  {
-    provide: USER_REPOSITORY,
-    useValue: User,
-  },
-];
