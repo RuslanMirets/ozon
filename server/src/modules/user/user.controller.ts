5e6995462fe91328633250fa6910c13efd3d8c68
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-02-25 12:04:58 +0300

    Guards [server]

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,10 +1,12 @@
 import { UserService } from 'src/modules/user/user.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
